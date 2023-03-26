f867e1d4241acc6ee31b78d189782f9a0b99bd2b
Jorge E Pasarell <jepasarell@gmail.com>
2023-03-25 22:14:16 -0400

    [refactor]

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,12 +1,10 @@
 export default function fizzbuzz(number: number): string {
-  if (number === 3 || number === 9 || number === 12) {
-    return "Fizz";
+  let result = "";
+  if (number % 3 === 0) {
+    result += "Fizz";
   }
-  if (number === 5) {
-    return "Buzz";
+  if (number % 5 === 0) {
+    result += "Buzz";
   }
-  if (number === 15) {
-    return "FizzBuzz";
-  }
-  return "";
+  return result;
 }
