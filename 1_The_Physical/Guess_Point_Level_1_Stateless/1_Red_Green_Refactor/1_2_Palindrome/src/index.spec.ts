d6c25f6dd08a034c99e66fc870ae3680dfdeff5e
Jorge E Pasarell <jepasarell@gmail.com>
2023-04-18 19:51:06 +0200

    [refactor] Remove redundancy in tests

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,19 +1,21 @@
 import PalindromeChecker from "./index";
 
+let palindromeChecker: PalindromeChecker;
+
+beforeEach(() => {
+  palindromeChecker = new PalindromeChecker();
+});
+
 describe("palindrome checker", () => {
   it("exists", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker).toBeDefined();
   });
 
   it("should know 'mom' is a palindrome", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.check("mom")).toBeTruthy();
   });
-  
+
   it("should know 'bill' is not a palindrome", () => {
-    const palindromeChecker = new PalindromeChecker();
     expect(palindromeChecker.check("bill")).toBeFalsy();
   });
-
 });
